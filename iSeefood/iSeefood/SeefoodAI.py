789b567e3e947617bebdad4c88fc31bd6b5a3010
Ibrahim <almohaimeed.6@wright.edu>
2019-04-28 14:43:23 +0300

    Add the iSeefood module

---
+++ iSeefood/iSeefood/SeefoodAI.py
@@ -0,0 +1,100 @@
+'''
+    ------------------- NOTE ---------------------------
+    THIS CLASS SHALL BE A SINGLETON. 
+    CURRENTLY, WE ONLY NEED A SINGLE INSTANCE
+    TO BE RUNNING. 
+    ----------------------------------------------------
+    [WHY?] AI OBJECT WILL CONSUME A LARGE AMOUNT OF MEMORY
+           THUS A SINGLE INSTANCE WOULD BE ENOUGH FOR NOW.
+    -------------- CREDIT ------------------------------
+    AI IS PROVIDED BY DR.DORAN FROM WRIGHT STATE
+    UNIVERSITY.  
+    -------------- AUTHOR ------------------------------
+            IBRAHIM ALMOHAIMEED 
+          ALMOHAIMEED.10@GMAIL.COM
+              XX/XX/2019
+    ----------------------------------------------------
+'''
+
+import argparse
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+import os
+
+
+class SeefoodAI(object):
+
+    def __init__(self):
+        ''' private constructor '''
+        self.sess = self.class_scores = self.x_input = self.keep_prob = None
+        self.scores = 0
+        self.__setup()
+
+    def __setup(self):
+        ''' Setting-up the SeefoodAI instance'''
+        # initializing the AI instance attrs, catch possible errors.
+        self.sess = tf.Session()
+        cwd = os.getcwd()
+        if os.path.exists(cwd+'/iSeefood/saved_model/model_epoch5.ckpt.meta'):
+            saver = tf.train.import_meta_graph(
+                cwd+'/iSeefood/saved_model/model_epoch5.ckpt.meta')
+            saver.restore(self.sess, tf.train.latest_checkpoint(
+                cwd+'/iSeefood/saved_model/'))
+            graph = tf.get_default_graph()
+            self.x_input = graph.get_tensor_by_name('Input_xn/Placeholder:0')
+            self.keep_prob = graph.get_tensor_by_name('Placeholder:0')
+            self.class_scores = graph.get_tensor_by_name("fc8/fc8:0")
+
+    def process(self, image_path):
+        ''' Process valid, existed file'''
+        if self.validatePath(image_path) and self.class_scores is not None:  # Validate given path.
+            # Open passed image, then convert it to RGB
+            image = Image.open(image_path).convert('RGB')
+            # Resize image to 227x227
+            image = image.resize((227, 227), Image.BILINEAR)
+            # Create a tensor
+            img_tensor = [np.asarray(image, dtype=np.float32)]
+            print '+ Looking for food in ' + image_path + ' ...... '
+            # Run the image in the model.
+            stat = self.sess.run(self.class_scores, {
+                self.x_input: img_tensor, self.keep_prob: 1.})
+            # Update score variable
+            self.__setScores(stat)
+            print("[--------------** Given Image Has Been Analyzed **----------------]")
+            # return true when an image is processed.
+            return True
+
+        return False
+
+    def validatePath(self, filePath):
+        ''' Validate given file path '''
+        # Verify that instance is a string type & !empty && Verify path existance && given path ends with .png || .jpg.
+        return isinstance(filePath, basestring) and self.validateExtension(filePath) and self.validateExistence(filePath)
+
+    def validateExistence(self, filePath):
+        ''' Verify the existance of the given path '''
+        return os.path.exists(filePath)  # retunr false otherwise
+
+    def validateExtension(self, filePath):
+        ''' Verify that the given path points to an image file (png, jpg) '''
+        if filePath.endswith('.png') or filePath.endswith('.jpg'):
+            return True  # return true if file is valide
+        return False  # return false otherwise
+
+    def __setScores(self, statistics):
+        ''' Allow the AI to set the score variable '''
+        # Set Score is not a public def., therefore no need to check data type.
+        self.scores = statistics
+
+    def getScores(self):
+        ''' Return last analyzed image stat. '''
+        return self.scores
+
+    def getResult(self, scores):
+        ''' Optimaze and generate a final score'''
+        # if np.argmax = 0; then the first class_score was higher, e.g., the model sees food.
+        # if np.argmax = 1; then the second class_score was higher, e.g., the model does not see food.
+        if np.argmax(scores) == 1:
+            return False
+        return True
---
+++ iSeefood/tests/SeefoodAI_test.py
@@ -0,0 +1,167 @@
+'''
+------------------ SeefoodAI class unit test -------------------
+
+'''
+import pytest
+import numpy
+from iSeefood.SeefoodAI import SeefoodAI
+import os
+
+SCOPE = "function"
+
+global current
+current = SeefoodAI()
+
+
+@pytest.fixture(scope=SCOPE)
+def shared_instance():
+    global current
+    yield current
+
+
+def test_getScores(shared_instance):
+    ''' TEST CASE: Getting the score unit before processing any image'''
+    # Get scores
+    result = shared_instance.getScores()
+    assert result == 0, "Score is none!"
+
+
+''' Test the Process unit '''
+''' Expected: only accepts valid file type & path '''
+
+
+def test_process1(shared_instance):
+    ''' TEST CASE: Pass valid file-type-path to be processed'''
+    cwd = os.getcwd()  # get current-working-directory
+    test = shared_instance.process(cwd+"/iSeefood/samples/cookies.png")
+    assert test is True, "Process1: Valid image to be processed." + cwd
+
+
+def test_process2(shared_instance):
+    ''' TEST CASE: Pass invalid file-type-path to be processed'''
+    cwd = os.getcwd()
+    test = shared_instance.process(cwd+"/invalid/samples/path.pg")
+    assert test is False, "Process2: Invalid image path/type to be processed."
+
+
+def test_process3(shared_instance):
+    ''' TEST CASE: Pass non-existed file-path to be processed'''
+    cwd = os.getcwd()
+    test = shared_instance.process(cwd+"/anywhere/file.png")
+    assert test is False, "Process3: Invalid image path to be processed."
+
+
+def test_process4(shared_instance):
+    ''' TEST CASE: Pass invalid file-type to be processed'''
+    cwd = os.getcwd()
+    test = shared_instance.process(cwd+"/iSeefood/samples/cookies.gif")
+    assert test is False, "Process4: Invalid image path to be processed."
+
+
+''' Test the Validation unit '''
+''' Expected: only accepts a non empty string'''
+
+
+def test_validatePath1(shared_instance):
+    ''' Pass an integar '''
+    pathState = shared_instance.validatePath(10)
+    assert pathState is False, "Parameter type Test #1"
+
+
+def test_validatePath2(shared_instance):
+    ''' Pass a string '''
+    cwd = os.getcwd()
+    pathState = shared_instance.validatePath(
+        cwd+"/iSeefood/samples/cookies.png")
+    assert pathState is True, "Parameter type Test #2"
+
+
+def test_validatePath3(shared_instance):
+    ''' Pass empty path'''
+    pathState = shared_instance.validatePath("")
+    assert pathState is False, "Parameter type Test #2"
+
+
+def test_validatePath4(shared_instance):
+    ''' Pass bool '''
+    pathState = shared_instance.validatePath(True)
+    assert pathState is False, "Parameter type Test #2"
+
+
+''' Test the directory existance checker unit '''
+''' Expected: Only existed directories are accepted '''
+
+
+def test_validateExistence(shared_instance):
+
+    # get current working directory
+    cwd = os.getcwd()
+    # Valid file path
+    result = shared_instance.validateExistence(
+        cwd+"/iSeefood/samples/cookies.png")
+    assert result is True, cwd + "/iSeefodd/samples/cookies.png directory exists"
+
+
+def test_validateExistence1(shared_instance):
+    # Invalid file path
+    result = shared_instance.validateExistence("samles/cookies.png")
+    assert result is False, "/home/ibrahim/Desktop/iSeefood/iSeefood/samples/cookies.png directory exists"
+
+
+''' Test file extension checker '''
+''' Expected: Only .png .jpg files are accepted '''
+
+
+def test_validateExtension(shared_instance):
+    # Valid file ext. PNG
+    result = shared_instance.validateExtension("image.png")
+    assert result is True, "PNG file extention test"
+
+
+def test_validateExtension1(shared_instance):
+    # Valid file ext. JPG
+    result = shared_instance.validateExtension("image.jpg")
+    assert result is True, "JPG file extention test"
+
+
+def test_validateExtension2(shared_instance):
+    # Invalid file ext. NO EXT.
+    result = shared_instance.validateExtension("image")
+    assert result is False, "None file extention test"
+
+
+def test_validateExtension3(shared_instance):
+    # Invalid file ext. Empty
+    result = shared_instance.validateExtension("")
+    assert result is False, "Empty file test"
+
+
+def test_getScores1(shared_instance):
+    ''' TEST CASE: Getting the score unit w/o setting the score'''
+    # Get scores
+    result = shared_instance.getScores()
+    assert isinstance(result, numpy.ndarray), "Score is none!"
+
+
+def test_getScores2(shared_instance):
+    ''' TEST CASE: getting the score unit w/ setting the score 
+        w/ a valid data type'''
+    # Get score.
+    result = shared_instance.getScores()
+    # Check that returned value is of type nm.ndarray.
+    assert isinstance(
+        result, numpy.ndarray), "Get score returned valid data type."
+
+
+def test_getResult(shared_instance):
+    lastStat = shared_instance.getScores()
+    assert shared_instance.getResult(lastStat) is True, "I see food in imahe"
+
+
+def test_getResult1(shared_instance):
+    cwd = os.getcwd()  # get current-working-directory
+    # run an image without food.
+    test = shared_instance.process(cwd+"/iSeefood/samples/poodle.png")
+    lastStat = shared_instance.getScores()
+    assert shared_instance.getResult(
+        lastStat) is False, "shared_instance.getScores()"
