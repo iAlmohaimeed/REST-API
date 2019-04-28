4ef881535becb5c5d96a710d5f8caa7ab9846e79
Ibrahim <almohaimeed.6@wright.edu>
2019-04-28 16:37:23 +0300

    Simplify the api code to make it clear and straightforward

--- server/server.py
+++ server/server.py
@@ -5,35 +5,21 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-
-
 class Employee(Resource):
     def get(self, employee_name):
         ''' GET REQUEST HANDLER FOR /employee/<employee_name> ENDPOINT '''
-        return 1
-    
-    def post(self, employee_name):
-        ''' POST REQUEST HANDLER FOR /employee/<employee_name> ENDPOINT '''
         return employee_name
 
-    
-class Job(Resource):
-    def get(self, employee_id):
-        ''' GET REQUEST HANDLER FOR /jobs/<employee_id> ENDPOINT '''
-        return 1
-
-    def post(self, employee_id):
-        ''' POST REQUEST HANDLER FOR /jobs/<employee_id> ENDPOINT '''
-        return 1
+    def post(self, employee_name):
+        ''' POST REQUEST HANDLER FOR /employee/<employee_name> ENDPOINT '''
+        return True
 
 '''
         ---- THE LIST OF ENDPOINTS ---- 
     Syntax:
-    api.add_resource(ClassName, '/endpointName')
+    api.add_resource(ClassName, '/endpointName/<parameter>')
 '''
-api.add_resource(Employee, '/employee/<employee_name>') # Endpoint #1
-api.add_resource(Job, '/jobs/<employee_id>') # Endpoint #2
-
+api.add_resource(Employee, '/employee/<employee_name>')  # Endpoint #1
 
 if __name__ == '__main__':
-     app.run()
+    app.run()
